69434dc5410dde64c9ad8c53e7860fb85bdd0b3f
sagar3034l <sd8778610@gmail.com>
2026-08-01 13:04:07 +0530

    Update CORS configuration to use CLIENT_URL and refactor API base URL to use environment variable

--- backend/app.js
+++ backend/app.js
@@ -11,7 +11,7 @@ const app = express();
 
 app.use(cookieParser())
 app.use(cors({
-  origin: "http://localhost:5173",
+  origin: process.env.CLIENT_URL,
   credentials: true
 }))
 
--- frontend/src/features/auth/services/auth.api.js
+++ frontend/src/features/auth/services/auth.api.js
@@ -1,7 +1,9 @@
 import axios from "axios";
 
+const BASE_URL = import.meta.env.VITE_API_URL || "http://localhost:3000/api";
+
 const axiosInstance = axios.create({
-    baseURL: "http://localhost:3000/api",
+    baseURL: BASE_URL,
     withCredentials: true
 });
 
